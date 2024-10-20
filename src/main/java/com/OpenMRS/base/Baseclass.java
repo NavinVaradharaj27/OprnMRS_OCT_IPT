63cfa17ae86d6681117d3c642b197c991937c1aa
maheswariguna <maheswari4010@gmail.com>
2024-10-20 12:42:19 +0530

    Browser launch created

--- src/main/java/com/OpenMRS/base/Baseclass.java
+++ src/main/java/com/OpenMRS/base/Baseclass.java
@@ -1,5 +1,24 @@
 package com.OpenMRS.base;
 
 public class Baseclass {
+	
+	protected void launchBrowser(String string) {
+	System.out.println("hai");
+		
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
 
 }
