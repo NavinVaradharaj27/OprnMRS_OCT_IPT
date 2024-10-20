b4bee7d486d46ef7ef320aec3c6fd2dc4c47c932
@KavithaSugumar1990 <kavithasugumarofficial@gmail.com>
2024-10-20 22:50:25 +0530

    assertion methd

--- src/main/java/com/OpenMRS/base/Baseclass.java
+++ src/main/java/com/OpenMRS/base/Baseclass.java
@@ -1,5 +1,14 @@
 package com.OpenMRS.base;
 
 public class Baseclass {
+	protected static void validation(String actual,String expected) {
+		try {
+			Assert.assertEquals(actual, expected);
+			
+		} catch (Exception e) {
+			Assert.fail("ERROR : OCCUR DURING VALIDATION ");
+			
+		}
+	}
 
 }
