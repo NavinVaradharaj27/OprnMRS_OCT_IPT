a318c5b0afdb80841a5f93d822e33257d2a1cab4
unknown <keerthivasan2510@gmail.com>
2024-10-20 12:42:27 +0530

    sendkays added

--- src/main/java/com/OpenMRS/base/Baseclass.java
+++ src/main/java/com/OpenMRS/base/Baseclass.java
@@ -1,5 +1,19 @@
 package com.OpenMRS.base;
 
+import org.openqa.selenium.WebElement;
+
 public class Baseclass {
+	
+        // sendkeys
+		public static void  sendKeys(WebElement element, String input) {
+			try {
+				element.sendKeys(input);
+				
+				
+			} catch (Exception e) {
+				e.printStackTrace();		
+				}
+			
+		}
 
 }
