f573310fb3f7f82c68e58ec899bfc0fcb3aa561e
NavinVaradharaj27 <navinv27@gmail.com>
2024-10-20 12:40:14 +0530

    Get attribute method added

--- src/main/java/com/OpenMRS/base/Baseclass.java
+++ src/main/java/com/OpenMRS/base/Baseclass.java
@@ -1,5 +1,13 @@
 package com.OpenMRS.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class Baseclass {
+	public WebDriver driver;
+	
+	public String getAttribute(WebElement element) {
+		return element.getAttribute("class");
+	}
 
 }
